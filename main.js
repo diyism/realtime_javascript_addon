8bf135fc5f434dcecd5758a8fde02fde5f1c1bb7
Malcolm Ke Win <kexianbin@diyism.com>
2024-02-25 02:02:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,39 @@ function $$(selector) {
 
 //$$('ytd-rich-section-renderer')[0].style.display = 'none';
 
-function hideCertainChannels() {
-$$('ytd-channel-name >* >* a').forEach(function(this_a_element) {
-  if (['马大哈动画', '神戳戳的苏神', '游戏解说艾登', '原野郎中', '哇哇哇游戏解说', '哇鸽鸽又来了', '校园小子',
+let channelNames = ['马大哈动画', '神戳戳的苏神', '游戏解说艾登', '原野郎中', '哇哇哇游戏解说', '哇鸽鸽又来了', '校园小子',
        '在下大沙雕', '暴走三金_', '猫小八动画', '芦苇草的梦想'
-      ].includes(this_a_element.innerText)) {
+      ];
+function updateChannelNames() {
+  fetch('https://gist.github.com/diyism/3b35aa863410b0a5e3cdf7bdd2c0c32b/raw/ad2b4f43f312974438f737348971e2771872855f/filter_youtube_channels.json')
+    .then(response => {
+      // 确保服务器响应成功
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // 假设JSON数据的格式是一个数组
+      channelNames = data;
+      console.log('Channel names updated:', channelNames);
+    })
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+}
+// 立即调用一次函数来初始化数组
+updateChannelNames();
+// 使用setInterval每2分钟调用一次updateChannelNames函数
+setInterval(updateChannelNames, 120000);
+
+function removeCertainChannels() {
+$$('ytd-channel-name >* >* a').forEach(function(this_a_element) {
+  if (channelNames.includes(this_a_element.innerText)) {
     console.log('removed');
     //this_a_element.closest('ytd-rich-item-renderer').style.display = 'none';
     this_a_element.closest('ytd-rich-item-renderer').remove();
   }
 });
 }
-
-setInterval(hideCertainChannels, 5000);
+setInterval(removeCertainChannels, 5000);
